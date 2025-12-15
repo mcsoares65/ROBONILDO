01ecd79e24cb0cf641f1c1b74eefd79d55551062
mcsoares65 <marciocubasoares@gmail.com>
2025-12-15 01:20:49 -0300

    Integrar envio de ordens com configuracao

---
+++ config.py
@@ -0,0 +1,9 @@
+"""
+Configurações globais do Robonildo.
+"""
+
+# Nome da janela do Profit que deve estar em foco para capturas e envio de ordens
+NOME_TELA_PROFIT = "ProfitPro"
+
+# Quando False, apenas simula as ordens sem enviar os atalhos para o Profit
+ENVIAR_ORDENS = False
